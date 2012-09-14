bb69f99312175b43aa329a50433915cf93343ab6
Alex Gray <alex@mrgray.com>
2012-09-13 20:14:37 -0400

    BlocksKit added as submodule

---
+++ AtoZ/Models/AZDock.h
@@ -0,0 +1,13 @@
+//
+//  AZDock.h
+//  AtoZ
+//
+//  Created by Alex Gray on 9/12/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import <AtoZ/AtoZ.h>
+
+@interface AZDock : BaseModel
+
+@end
---
+++ AtoZ/Models/AZDock.m
@@ -0,0 +1,13 @@
+//
+//  AZDock.m
+//  AtoZ
+//
+//  Created by Alex Gray on 9/12/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import "AZDock.h"
+
+@implementation AZDock
+
+@end
