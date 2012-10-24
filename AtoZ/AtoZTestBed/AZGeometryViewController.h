8e30d120dfeca7919264606a174b3a4c999fa9e3
Alex Gray <alex@mrgray.com>
2012-10-24 12:59:55 -0400

    new view controller

---
+++ AtoZ/AtoZTestBed/AZGeometryViewController.h
@@ -0,0 +1,13 @@
+//
+//  AZGeometryViewController.h
+//  AtoZ
+//
+//  Created by Alex Gray on 10/24/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AZGeometryViewController : NSViewController
+
+@end
---
+++ AtoZ/AtoZTestBed/AZGeometryViewController.m
@@ -0,0 +1,27 @@
+//
+//  AZGeometryViewController.m
+//  AtoZ
+//
+//  Created by Alex Gray on 10/24/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import "AZGeometryViewController.h"
+
+@interface AZGeometryViewController ()
+
+@end
+
+@implementation AZGeometryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
