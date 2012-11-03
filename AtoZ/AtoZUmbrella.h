4288c65c8da0f3d275f6e52ee1b48ca64cfc03be
Alex Gray <alex@mrgray.com>
2012-11-02 20:40:17 -0400

    Drawkit

--- AtoZ/AtoZUmbrella.h
+++ AtoZ/AtoZUmbrella.h
@@ -223,6 +223,7 @@ AZLOG(@"<INTERNAL INCONSISTENCY>"); \
 #define bgC backgroundColor
 #define fgC foregroundColor
 #define arMASK autoresizingMask
+#define NDOBC needsDisplayOnBoundsChange
 #define nDoBC needsDisplayOnBoundsChange
 #define CASIZEABLE kCALayerWidthSizable | kCALayerHeightSizable
 #define NSSIZEABLE NSViewHeightSizable | NSViewWidthSizable
@@ -391,6 +392,57 @@ attr1 relativeTo:relName attribute:attr2 scale:scl offset:off]
 // RAND_DIRECTION() -1 or +1 (usage: int steps = 10*RAND_DIRECTION();  will get you -10 or 10)
 #define RAND_BOOL()				(arc4random() & 1)
 #define RAND_DIRECTION()		(RAND_BOOL() ? 1 : -1)
+
+
+
+
+
+// pinning a value between a lower and upper limit
+
+#define LIMIT( value, min, max )		(((value) < (min))? (min) : (((value) > (max))? (max) : (value)))
+
+// converting from radians to degrees
+
+#define	DEGREES_TO_RADIANS( d )			((d) * 0.0174532925199432958)
+#define RADIANS_TO_DEGREES( r )			((r) * 57.29577951308232)
+
+// some useful angular constants
+
+#define FIFTEEN_DEGREES					(0.261799387799)
+#define NINETY_DEGREES					(pi * 0.5)
+#define FORTYFIVE_DEGREES				(pi * 0.25)
+#define HALF_PI							(pi * 0.5)
+
+// exception safe save/restore of the current graphics context
+
+#define	SAVE_GRAPHICS_CONTEXT			@try { [NSGraphicsContext saveGraphicsState];
+#define RESTORE_GRAPHICS_CONTEXT		} @finally { [NSGraphicsContext restoreGraphicsState]; }
+
+
+// 64-bit float macros
+
+#ifdef __LP64__
+	#define _CGFloatFabs( n )	fabs( n )
+	#define _CGFloatTrunc( n )	trunc( n )
+	#define _CGFloatLround( n )	roundtol( n )
+	#define _CGFloatFloor( n )	floor( n )
+	#define _CGFloatCeil( n )	ceil( n )
+	#define _CGFloatExp( n )	exp( n )
+	#define _CGFloatSqrt( n )	sqrt( n )
+	#define _CGFloatLog( n )	log( n )
+#else
+	#define _CGFloatFabs( n )	fabsf( n )
+	#define _CGFloatTrunc( n )	truncf( n )
+	#define _CGFloatLround( n )	roundtol((double) n )
+	#define _CGFloatFloor( n )	floorf( n )
+	#define _CGFloatCeil( n )	ceilf( n )
+	#define _CGFloatExp( n )	expf( n )
+	#define _CGFloatSqrt( n )	sqrtf( n )
+	#define _CGFloatLog( n )	logf( n )
+#endif
+
+
+
 #define $point(A)       	[NSValue valueWithPoint:A]
 #define $points(A,B)       	[NSValue valueWithPoint:CGPointMake(A,B)]
 #define $rect(A,B,C,D)    	[NSValue valueWithRect:CGRectMake(A,B,C,D)]
--- AtoZ/Categories/NSBezierPath+AtoZ.h
+++ AtoZ/Categories/NSBezierPath+AtoZ.h
@@ -153,3 +153,160 @@ typedef enum {
 - (NSPoint) drawPathElement:(int) n withPreviousPoint: (NSPoint) previous;
 - (NSPoint) drawPathElement:(int) n  withPreviousPoint: (NSPoint) previous inColor: (NSColor*) pointColor withHandlesInColor: (NSColor*) handleColor;
 @end
+
+
+///**********************************************************************************************************************************
+///  NSBezierPath-Geometry.h
+///  DrawKit �2005-2008 Apptree.net
+///
+///  Created by graham on 22/10/2006.
+
+
+
+@interface NSBezierPath (Geometry)
+
+// simple transformations
+
+- (NSBezierPath*)		scaledPath:(float) scale;
+- (NSBezierPath*)		scaledPath:(float) scale aboutPoint:(NSPoint) cp;
+- (NSBezierPath*)		rotatedPath:(float) angle;
+- (NSBezierPath*)		rotatedPath:(float) angle aboutPoint:(NSPoint) cp;
+- (NSBezierPath*)		insetPathBy:(float) amount;
+- (NSBezierPath*)		horizontallyFlippedPathAboutPoint:(NSPoint) cp;
+- (NSBezierPath*)		verticallyFlippedPathAboutPoint:(NSPoint) cp;
+- (NSBezierPath*)		horizontallyFlippedPath;
+- (NSBezierPath*)		verticallyFlippedPath;
+
+- (NSPoint)				centreOfBounds;
+- (float)				minimumCornerAngle;
+
+// iterating over a path using a iteration delegate:
+
+- (NSBezierPath*)		bezierPathByIteratingWithDelegate:(id) delegate contextInfo:(void*) contextInfo;
+
+- (NSBezierPath*)		paralleloidPathWithOffset:(float) delta;
+- (NSBezierPath*)		paralleloidPathWithOffset2:(float) delta;
+- (NSBezierPath*)		paralleloidPathWithOffset22:(float) delta;
+- (NSBezierPath*)		offsetPathWithStartingOffset:(float) delta1 endingOffset:(float) delta2;
+- (NSBezierPath*)		offsetPathWithStartingOffset2:(float) delta1 endingOffset:(float) delta2;
+
+// interpolating flattened paths:
+
+- (NSBezierPath*)		bezierPathByInterpolatingPath:(float) amount;
+
+// calculating a fillet
+
+- (NSBezierPath*)		filletPathForVertex:(NSPoint[]) vp filletSize:(float) fs;
+
+// roughening and randomising paths
+
+- (NSBezierPath*)		bezierPathByRandomisingPoints:(float) maxAmount;
+#if 0
+- (NSBezierPath*)		bezierPathWithRoughenedStrokeOutline:(float) amount;
+#endif
+- (NSBezierPath*)		bezierPathWithFragmentedLineSegments:(float) flatness;
+
+// zig-zags and waves
+
+- (NSBezierPath*)		bezierPathWithZig:(float) zig zag:(float) zag;
+- (NSBezierPath*)		bezierPathWithWavelength:(float) lambda amplitude:(float) amp spread:(float) spread;
+
+// getting the outline of a stroked path:
+#if 0
+- (NSBezierPath*)		strokedPath;
+- (NSBezierPath*)		strokedPathWithStrokeWidth:(float) width;
+#endif
+// breaking a path apart:
+
+- (NSArray*)			subPaths;
+- (int)					countSubPaths;
+
+// converting to and from Core Graphics paths
+
+#if 0
+- (CGPathRef)			newQuartzPath;
+- (CGMutablePathRef)	newMutableQuartzPath;
+- (CGContextRef)		setQuartzPath;
+- (void)				setQuartzPathInContext:(CGContextRef) context isNewPath:(BOOL) np;
+
++ (NSBezierPath*)		bezierPathWithCGPath:(CGPathRef) path;
++ (NSBezierPath*)		bezierPathWithPathFromContext:(CGContextRef) context;
+#endif
+- (NSPoint)				pointOnPathAtLength:(float) length slope:(float*) slope;
+- (float)				slopeStartingPath;
+- (float)				distanceFromStartOfPathAtPoint:(NSPoint) p tolerance:(float) tol;
+
+- (int)					pointWithinPathRegion:(NSPoint) p;
+
+
+// clipping utilities:
+#if 0
+- (void)				addInverseClip;
+#endif
+// path trimming
+
+- (float)				lengthOfElement:(int) i;
+- (float)				lengthOfPathFromElement:(int) startElement toElement:(int) endElement;
+
+- (NSPoint)				firstPoint;
+- (NSPoint)				lastPoint;
+
+// trimming utilities - modified source originally from A J Houghton, see copyright notice below
+
+- (NSBezierPath*)		bezierPathByTrimmingToLength:(float) trimLength;
+- (NSBezierPath*)		bezierPathByTrimmingToLength:(float) trimLength withMaximumError:(float) maxError;
+
+- (NSBezierPath*)		bezierPathByTrimmingFromLength:(float) trimLength;
+- (NSBezierPath*)		bezierPathByTrimmingFromLength:(float) trimLength withMaximumError:(float) maxError;
+
+- (NSBezierPath*)		bezierPathByTrimmingFromBothEnds:(float) trimLength;
+- (NSBezierPath*)		bezierPathByTrimmingFromBothEnds:(float) trimLength withMaximumError:(float) maxError;
+
+- (NSBezierPath*)		bezierPathByTrimmingFromCentre:(float) trimLength;
+- (NSBezierPath*)		bezierPathByTrimmingFromCentre:(float) trimLength withMaximumError:(float) maxError;
+
+- (NSBezierPath*)		bezierPathByTrimmingFromLength:(float) startLength toLength:(float) newLength;
+- (NSBezierPath*)		bezierPathByTrimmingFromLength:(float) startLength toLength:(float) newLength withMaximumError:(float) maxError;
+
+- (NSBezierPath*)		bezierPathWithArrowHeadForStartOfLength:(float) length angle:(float) angle closingPath:(BOOL) closeit;
+- (NSBezierPath*)		bezierPathWithArrowHeadForEndOfLength:(float)length angle:(float) angle closingPath:(BOOL) closeit;
+
+- (void)				appendBezierPathRemovingInitialMoveToPoint:(NSBezierPath*) path;
+
+- (float)				length;
+- (float)				lengthWithMaximumError:(float) maxError;
+
+@end
+
+
+
+// informal protocol for iterating over the elements in a bezier path using bezierPathByIteratingWithDelegate:contextInfo:
+
+@interface NSObject (BezierElementIterationDelegate)
+
+- (void)				path:(NSBezierPath*) path			// the new path that the delegate can build or modify from the information given
+						elementIndex:(int) element			// the element index 
+						type:(NSBezierPathElement) type		// the element type
+						points:(NSPoint*) p					// list of associated points 0 = next point, 1 = cp1, 2 = cp2 (for curves), 3 = last point on subpath
+						subPathIndex:(int) spi				// which subpath this is
+						subPathClosed:(BOOL) spClosed		// is the subpath closed?
+						contextInfo:(void*) contextInfo;	// the context info
+
+
+@end
+
+// undocumented Core Graphics:
+#if 0
+extern CGPathRef	CGContextCopyPath( CGContextRef context );
+#endif
+
+/*
+ * Bezier path utility category (trimming)
+ *
+ * (c) 2004 Alastair J. Houghton
+ * All Rights Reserved.
+ *
+*/
+void subdivideBezierAtT(const NSPoint bez[4], NSPoint bez1[4], NSPoint bez2[4], float t);
+
+
