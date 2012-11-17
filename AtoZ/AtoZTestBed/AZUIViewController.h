c1a2cb5a2019edbb0e4a71460f174865a5f4a721
Alex Gray <alex@mrgray.com>
2012-11-17 00:24:42 -0500

    mainline commit.

---
+++ AtoZ/AtoZTestBed/AZUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  AZUIViewController.h
+//  AtoZ
+//
+//  Created by Alex Gray on 11/16/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AZUIViewController : NSViewController
+
+@end
---
+++ AtoZ/AtoZTestBed/AZUIViewController.m
@@ -0,0 +1,27 @@
+//
+//  AZUIViewController.m
+//  AtoZ
+//
+//  Created by Alex Gray on 11/16/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import "AZUIViewController.h"
+
+@interface AZUIViewController ()
+
+@end
+
+@implementation AZUIViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
