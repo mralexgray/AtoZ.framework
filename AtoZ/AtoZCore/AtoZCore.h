96911b94529be96127406b6328a50aa8c198ca43
Alex Gray <alex@mrgray.com>
2013-01-18 16:59:48 -0500

    core branch sync

---
+++ AtoZ/AtoZCore/AtoZCore.h
@@ -0,0 +1,13 @@
+//
+//  AtoZCore.h
+//  AtoZCore
+//
+//  Created by Alex Gray on 12/14/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AtoZCore : NSObject
+
+@end
---
+++ AtoZ/AtoZCore/AtoZCore.m
@@ -0,0 +1,13 @@
+//
+//  AtoZCore.m
+//  AtoZCore
+//
+//  Created by Alex Gray on 12/14/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import "AtoZCore.h"
+
+@implementation AtoZCore
+
+@end
