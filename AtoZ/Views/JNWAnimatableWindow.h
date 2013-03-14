ed65224cf9ac87eebf391382ff676cb62d6e5c9c
Alex Gray <alex@mrgray.com>
2013-03-13 20:34:36 -0400

    CLI httpresponder class.

---
+++ AtoZ/Views/JNWAnimatableWindow.h
@@ -0,0 +1,118 @@
+
+/* 	Allows for an extremely flexible manipulation of a static representation of the window.
+ 	Since it uses a visual representation of the window, the window cannot be interacted with while a transform is applied, nor is it automatically updated to reflect the window's state.
+
+EXAMPLE:
+
+	CABA *opacity 		= [CABA animationWithKeyPath:@"opacity"];					opacity.toValue 		= @0;
+	CABA *translation 	= [CABA animationWithKeyPath:@"transform.translation.y"];	translation.toValue 		= @(-50);
+	
+	CAAG *group 		 = CAAG.animation;
+	group.timingFunction = CAMEDIAEASY;
+	group.animations 	 = @[ opacity, translation ];
+	group.duration 		 = self.animationDuration;
+	[self.window orderOutWithAnimation:group];															or
+
+	[self.window setFrame:rect withDuration:self.animationDuration timing:nil];							or
+
+	CATransform3D transform = CATransform3DIdentity; 	transform.m34 = -1.f / 700.f;	// Apply a perspective transform onto the layer.
+	self.window.layer.transform = transform;
+	
+	CABA *animation = [CABA animationWithKeyPath:@"transform.rotation.x"]; 				// Do a barrel roll.
+	animation.duration = 3.f;						
+	animation.toValue = @(2*M_PI);
+	animation.timingFunction = CAMEDIAEASY;
+	animation.delegate = self;															// Set the delegate on the animation so we know when to remove the fake window.
+	[self.window.layer addAnimation:animation forKey:nil];
+	
+	which calls....
+
+	- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag   { [self.window destroyTransformingWindow];  }
+	
+	Animation is done, so lets get back to our real window by destroying the fake one.
+	Note this only needs to happen if we initiate the creation of the layer ourselves by referencing it, 
+	otherwise it is automatically destroyed for us, as mentioned above.
+*/
+@interface JNWAnimatableWindow : NSWindow
+
+/*	This layer can be transformed as much as desired. As soon as the property is first used an image representation of the current window's state will be grabbed and used for the layer's contents.
+	Because it is a static image, it will not reflect the state of the window if it changes.
+	If the window needs to change content while still having a transformed state, call `-updateImageRepresentation` to update the backing image.
+
+	Important note: This layer is optimized with a shadow path. If you need to change the bounds or the size of the layer, you will need to update the shadow path yourself to reflect this change.
+*/
+@property (nonatomic, assign, readonly) CALayer *layer;
+
+/* 	Destroys the layer and fake window. Only necessary for use if the layer is animated manually.
+	If the convenience methods are used below, calling this is not necessary as it is done automatically. */
+
+- (void)destroyTransformingWindow;
+
+/* 	Order a window out with an animation. The `animations` block is wrapped in a `CATransaction`, so implicit
+ 	animations will be enabled. Pass in nil for the timing function to default to ease-in-out.
+ 	The layer and the extra window will be destroyed automatically after the animation completes. */
+
+- (void)orderOutWithDuration:(CFTimeInterval)duration timing:(CAMediaTimingFunction *)timingFunction
+				  animations:(void (^)(CALayer *windowLayer))animations;
+
+/* 	Order a window out with an animation, automatically cleaning up after completion. The delegate of the animation will be changed. */
+
+- (void)orderOutWithAnimation:(CAAnimation *)animation;
+
+/* 	Make a window key and visible with an animation. The setup block will be performed with implicit animations
+	disabled, so it is an ideal time to set the initial state for your animation. The `animations` block is wrapped
+	in a `CATransaction`, so implicit animations will be enabled. Pass in nil for the timing function to default to ease-in-out.
+	The layer and the extra window will be destroyed automatically after the animation completes. */
+
+- (void)makeKeyAndOrderFrontWithDuration:(CFTimeInterval)duration timing:(CAMediaTimingFunction *)timingFunction
+								   setup:(void (^)(CALayer *windowLayer))setup animations:(void (^)(CALayer *layer))animations;
+/*	EXAMPLE:
+
+	[self.window makeKeyAndOrderFrontWithDuration:self.animationDuration timing:nil setup:^(CALayer *layer) {
+
+		//	Anything done in this setup block is performed without any animation.
+		//	The layer will not be visible during this time so now is our chance to set initial values for opacity, transform, etc.
+
+		layer.transform = CATransform3DMakeTranslation(0.f, -50., 0.f);
+		layer.opacity = 0.f;													} animations:^(CALayer *layer) {
+	
+		// 	Now we're actually animating. In order to make the transition as seamless as possible, we want to set the final values to
+		//	their original states, so that when the fake window is removed there will be no discernible jump to that state.
+		// 	To change the default timing and duration, just wrap the animations in an NSAnimationContext.
+		
+		layer.transform = CATransform3DIdentity;
+		layer.opacity = 1.f;
+	}];
+
+	Make a window key and visible with an animation, automatically cleaning up after completion.
+	The delegate of the animation will be changed.
+	The opacity of the layer will be set to the passed in opacity before it is shown. */
+
+- (void)makeKeyAndOrderFrontWithAnimation:(CAAnimation *)animation initialOpacity:(CGFloat)opacity;
+
+/*	Sets the window to the frame specified using a layer The animation behavior is the same as
+	NSWindow's full-screen animation, which cross-fades between the initial and final state images.
+	The layer and the extra window will be destroyed automatically after the animation completes. */
+- (void)setFrame:(NSRect)frameRect withDuration:(CFTimeInterval)duration timing:(CAMediaTimingFunction *)timing;
+
+@end
+
+
+/*
+ Copyright (c) 2013, Jonathan Willing. All rights reserved.
+ Licensed under the MIT license <http://opensource.org/licenses/MIT>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+ to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ IN THE SOFTWARE.
+ */
+
