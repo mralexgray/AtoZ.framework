1aed1c39a7d98f2b998dbab1a5eeb0e13950f46c
Alex Gray <alex@mrgray.com>
2013-05-06 10:58:15 -0400

    checking in.

---
+++ AtoZ/Models/AtoZContacts.h
@@ -0,0 +1,13 @@
+//
+//  AtoZContacts.h
+//  AtoZ
+//
+//  Created by Alex Gray on 5/4/13.
+//  Copyright (c) 2013 mrgray.com, inc. All rights reserved.
+//
+
+#import <AtoZ/AtoZ.h>
+
+@interface AtoZContacts : BaseModel
+
+@end
---
+++ AtoZ/Models/AtoZContacts.m
@@ -0,0 +1,13 @@
+//
+//  AtoZContacts.m
+//  AtoZ
+//
+//  Created by Alex Gray on 5/4/13.
+//  Copyright (c) 2013 mrgray.com, inc. All rights reserved.
+//
+
+#import "AtoZContacts.h"
+
+@implementation AtoZContacts
+
+@end
