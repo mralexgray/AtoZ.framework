90b2b8d72f9ee66969803b2d5e03909788cbe10c
Alex Gray <alex@mrgray.com>
2013-05-27 10:25:20 -0400

    checking in

---
+++ AtoZ/Views/AZWindowTab.h
@@ -0,0 +1,13 @@
+//
+//  AZWindowTab.h
+//  AtoZ
+//
+//  Created by Alex Gray on 5/21/13.
+//  Copyright (c) 2013 mrgray.com, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AZWindowTab : NSObject
+
+@end
---
+++ AtoZ/Views/AZWindowTab.m
@@ -0,0 +1,13 @@
+//
+//  AZWindowTab.m
+//  AtoZ
+//
+//  Created by Alex Gray on 5/21/13.
+//  Copyright (c) 2013 mrgray.com, inc. All rights reserved.
+//
+
+#import "AZWindowTab.h"
+
+@implementation AZWindowTab
+
+@end
