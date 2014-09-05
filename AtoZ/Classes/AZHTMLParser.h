2e9be6b7471baed1bb4f586ae27ac51e015953ae
Alex Gray <alex@mrgray.com>
2014-09-04 20:36:30 -0400

    rezhuzhed header.

--- AtoZ/Classes/AZHTMLParser.h
+++ AtoZ/Classes/AZHTMLParser.h
@@ -2,21 +2,17 @@
 //  HTMLParser.h  StackOverflow
 //  Created by Ben Reeves on 09/03/2010.  Copyright 2010 Ben Reeves. All rights reserved.
 
-
-#import <Foundation/Foundation.h>
-
 @class           HTMLNode ;
 @interface 	 AZHTMLParser : NSObject
 
--(id)initWithContentsOfURL:(NSURL*)url 		error:(NSError**)error;
--(id)initWithData:			(NSData*)data 		error:(NSError**)error;
--(id)initWithString:			(NSString*)string error:(NSError**)error;
+- initWithContentsOfURL:(NSU*)u error:(NSERR**)e;
+-          initWithData:(DTA*)d error:(NSERR**)e;
+-        initWithString:(NSS*)s error:(NSERR**)e;
 
-- (HTMLNode*)doc;		// Returns the doc  tag
-- (HTMLNode*)body;	//	Returns the body tag
-- (HTMLNode*)html; 	//	Returns the html tag
-- (HTMLNode*)head;	//	Returns the head tag
-
+@prop_RO HTMLNode * doc,		// Returns the doc  tag
+                  * body,	//	Returns the body tag
+                  * html, 	//	Returns the html tag
+                  * head;	//	Returns the head tag
 @end
 
 
